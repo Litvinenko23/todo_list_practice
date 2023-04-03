ede62010c998fb1bd6c3471e2c75b335ec1fafa3
Litvinenko23 <andreylitvinenko20102010@gmail.com>
2023-04-03 13:37:55 +0300

    Added buttons for update/delete task

--- todo/urls.py
+++ todo/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("tags/<int:pk>delete/", TagDeleteView.as_view(), name="tag-delete"),
     path("create-task/", TaskCreateView.as_view(), name="task-create"),
     path("update-task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("delete-task/<int:pk>delete/", TaskDeleteView.as_view(), name="tag]sk-delete"),
+    path("delete-task/<int:pk>delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 
